255b56a5496f0c85c23d6f29813f8ca020b61599
José María Sola Durán <josemaria.sola.duran@gmail.com>
2025-06-17 16:40:11 +0200

    Fixed redirect_uri

--- backend/src/crypto_trailing_stop/interfaces/controllers/login_controller.py
+++ backend/src/crypto_trailing_stop/interfaces/controllers/login_controller.py
@@ -4,13 +4,15 @@ from typing import Annotated
 from crypto_trailing_stop.interfaces.dtos.login_dto import LoginDto
 from crypto_trailing_stop.interfaces.telegram.services import TelegramService
 from crypto_trailing_stop.interfaces.telegram.keyboards_builder import KeyboardsBuilder
-from crypto_trailing_stop.config import get_oauth_context
+from crypto_trailing_stop.config import get_oauth_context, get_configuration_properties
 from crypto_trailing_stop.commons.constants import AUTHORIZED_GOOGLE_USER_EMAILS
+from urllib.parse import urlunparse, urlparse
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/login", tags=["login"])
 
+configuration_properties = get_configuration_properties()
 telegram_service = TelegramService()
 keyboards_builder = KeyboardsBuilder()
 
@@ -29,7 +31,20 @@ async def login(
         request.session["login_query_params"] = login_query_params.model_dump(
             mode="python"
         )
-        redirect_uri = request.url_for("login_callback")
+        # TODO: Refator this to use a more generic URL building method
+        parsed_public_domain = urlparse(configuration_properties.public_domain)
+        parsed_login_callback_url = request.url_for("login_callback")
+        redirect_uri = urlunparse(
+            (
+                parsed_public_domain.scheme,
+                parsed_public_domain.netloc,
+                parsed_login_callback_url.path,
+                "",
+                parsed_login_callback_url.query,
+                parsed_login_callback_url.fragment,
+            )
+        )
+        logger.info(f"Redirecting to Google OAuth with redirect URI: {redirect_uri}")
         oauth_context = get_oauth_context()
         google_auth = oauth_context.create_client("google")
         response = await google_auth.authorize_redirect(request, redirect_uri)
@@ -53,7 +68,7 @@ async def login_callback(request: Request) -> Response:
         token = await google_auth.authorize_access_token(request)
         userinfo = request.session["userinfo"] = token["userinfo"]
         if userinfo["email"] not in AUTHORIZED_GOOGLE_USER_EMAILS:
-            logger.warning(f"Unauthorized user: {token['email']}")
+            logger.warning(f"Unauthorized user: {userinfo['email']}")
             await telegram_service.send_message(
                 chat_id=login_query_params.tg_chat_id,
                 text="Unauthorized user. Please contact the administrator.",
