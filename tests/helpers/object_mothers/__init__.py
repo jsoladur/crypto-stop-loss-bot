8005d3f047740532b5d788ad963a65a776e23e3a
José María Sola Durán <josemaria.sola.duran@gmail.com>
2025-07-01 13:11:50 +0200

    Increasing coverage (WIP)

--- tests/helpers/object_mothers/__init__.py
+++ tests/helpers/object_mothers/__init__.py
@@ -1,5 +1,11 @@
 from tests.helpers.object_mothers.bit2me_order_dto_object_mother import Bit2MeOrderDtoObjectMother
 from tests.helpers.object_mothers.bit2me_summary_xlsx_object_mother import Bit2MeSummaryXlsxObjectMother
 from tests.helpers.object_mothers.bit2me_tickers_dto_object_mother import Bit2MeTickersDtoObjectMother
+from tests.helpers.object_mothers.bit2me_trade_dto_object_mother import Bit2MeTradeDtoObjectMother
 
-__all__ = ["Bit2MeOrderDtoObjectMother", "Bit2MeTickersDtoObjectMother", "Bit2MeSummaryXlsxObjectMother"]
+__all__ = [
+    "Bit2MeOrderDtoObjectMother",
+    "Bit2MeTickersDtoObjectMother",
+    "Bit2MeSummaryXlsxObjectMother",
+    "Bit2MeTradeDtoObjectMother",
+]
