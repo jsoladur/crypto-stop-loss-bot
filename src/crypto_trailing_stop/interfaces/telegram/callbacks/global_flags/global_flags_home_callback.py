c80a77e35e9db9128af838df4d4d3e192ca9bcfc
José María Sola Durán <josemaria.sola.duran@gmail.com>
2025-07-19 14:09:17 +0200

    Added exceptions handler to Telegram stop_loss_percent_home_callback_handler

--- src/crypto_trailing_stop/interfaces/telegram/callbacks/global_flags/global_flags_home_callback.py
+++ src/crypto_trailing_stop/interfaces/telegram/callbacks/global_flags/global_flags_home_callback.py
@@ -1,5 +1,7 @@
 import logging
+from html import escape as html_escape
 
+from aiogram import html
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
 
@@ -20,11 +22,17 @@ global_flag_service = GlobalFlagService()
 async def stop_loss_percent_home_callback_handler(callback_query: CallbackQuery, state: FSMContext) -> None:
     is_user_logged = await session_storage_service.is_user_logged(state)
     if is_user_logged:
-        global_flag_items = await global_flag_service.find_all()
-        await callback_query.message.answer(
-            "ℹ️ Click into a flag to active/deactivate them",
-            reply_markup=keyboards_builder.get_global_flags_home_keyboard(global_flag_items),
-        )
+        try:
+            global_flag_items = await global_flag_service.find_all()
+            await callback_query.message.answer(
+                "ℹ️ Click into a flag to active/deactivate them",
+                reply_markup=keyboards_builder.get_global_flags_home_keyboard(global_flag_items),
+            )
+        except Exception as e:
+            logger.error(f"Error trying to retrieve global flags: {str(e)}", exc_info=True)
+            await callback_query.message.answer(
+                f"⚠️ An error occurred while retrieving global flags. Please try again later:\n\n{html.code(html_escape(str(e)))}"  # noqa: E501
+            )
     else:
         await callback_query.message.answer(
             "⚠️ Please log in to operate with Global Flags.", reply_markup=keyboards_builder.get_login_keyboard(state)
