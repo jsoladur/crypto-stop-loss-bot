952eb10ea3a9fac2a3fa2166bf906c35437b45a5
José María Sola Durán <josemaria.sola.duran@gmail.com>
2025-07-20 14:20:35 +0200

    Fixed tests and better Readme.md file

--- src/crypto_trailing_stop/config.py
+++ src/crypto_trailing_stop/config.py
@@ -30,7 +30,7 @@ _dispacher: Dispatcher | None = None
 class _CustomEnvSettingsSource(EnvSettingsSource):
     def prepare_field_value(self, field_name: str, field: FieldInfo, value: Any, value_is_complex: bool) -> Any:
         if field_name == "authorized_google_user_emails_comma_separated":
-            ret = [str(v).strip().lower() for v in value.split(",")]
+            ret = [str(v).strip().lower() for v in value.split(",")] if value else None
         else:
             ret = super().prepare_field_value(field_name, field, value, value_is_complex)
         return ret
