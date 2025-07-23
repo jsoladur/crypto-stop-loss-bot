d242c511ef94a02e40f73fb3108aceaaae584ee4
José María Sola Durán <josemaria.sola.duran@gmail.com>
2025-07-23 14:27:29 +0200

    Added better UX in Telegram current metrics and adding more logic to CryptoMarketMetrics class

--- src/crypto_trailing_stop/infrastructure/services/vo/crypto_market_metrics.py
+++ src/crypto_trailing_stop/infrastructure/services/vo/crypto_market_metrics.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 import pandas as pd
 
@@ -10,9 +11,10 @@ from crypto_trailing_stop.config import get_configuration_properties
 from crypto_trailing_stop.infrastructure.tasks.vo.types import RSIState
 
 
-@dataclass
+@dataclass(frozen=True)
 class CryptoMarketMetrics:
     symbol: str
+    timestamp: datetime
     closing_price: float | int
     ema_short: float | int
     ema_mid: float | int
@@ -23,6 +25,8 @@ class CryptoMarketMetrics:
     adx: float | int
     adx_pos: float | int
     adx_neg: float | int
+
+    is_rounded: bool
 
     @property
     def rsi_state(self) -> RSIState:
@@ -43,11 +47,33 @@ class CryptoMarketMetrics:
             ndigits=NUMBER_OF_DECIMALS_IN_PRICE_BY_SYMBOL.get(self.symbol, DEFAULT_NUMBER_OF_DECIMALS_IN_PRICE),
         )
 
+    def rounded(self) -> "CryptoMarketMetrics":
+        ret = self
+        if not self.is_rounded:
+            ndigits = NUMBER_OF_DECIMALS_IN_PRICE_BY_SYMBOL.get(self.symbol, DEFAULT_NUMBER_OF_DECIMALS_IN_PRICE)
+            ret = CryptoMarketMetrics(
+                symbol=self.symbol,
+                timestamp=self.timestamp,
+                closing_price=round(self.closing_price, ndigits=ndigits),
+                ema_short=round(self.ema_short, ndigits=ndigits),
+                ema_mid=round(self.ema_mid, ndigits=ndigits),
+                ema_long=round(self.ema_long, ndigits=ndigits),
+                macd_hist=round(self.macd_hist, ndigits=4),
+                rsi=round(self.rsi, ndigits=2),
+                atr=round(self.atr, ndigits=ndigits),
+                adx=round(self.adx, ndigits=2),
+                adx_pos=round(self.adx_pos, ndigits=2),
+                adx_neg=round(self.adx_neg, ndigits=2),
+                is_rounded=True,
+            )
+        return ret
+
     @staticmethod
     def from_candlestick(symbol: str, candlestick: pd.Series, *, apply_round: bool = False) -> "CryptoMarketMetrics":
         ndigits = NUMBER_OF_DECIMALS_IN_PRICE_BY_SYMBOL.get(symbol, DEFAULT_NUMBER_OF_DECIMALS_IN_PRICE)
         ret = CryptoMarketMetrics(
             symbol=symbol,
+            timestamp=candlestick["timestamp"],
             closing_price=round(candlestick["close"], ndigits=ndigits) if apply_round else candlestick["close"],
             ema_short=round(candlestick["ema_short"], ndigits=ndigits) if apply_round else candlestick["ema_short"],
             ema_mid=round(candlestick["ema_mid"], ndigits=ndigits) if apply_round else candlestick["ema_mid"],
@@ -58,5 +84,6 @@ class CryptoMarketMetrics:
             adx=round(candlestick["adx"], ndigits=2) if apply_round else candlestick["adx"],
             adx_pos=round(candlestick["adx_pos"], ndigits=2) if apply_round else candlestick["adx_pos"],
             adx_neg=round(candlestick["adx_neg"], ndigits=2) if apply_round else candlestick["adx_neg"],
+            is_rounded=apply_round,
         )
         return ret
