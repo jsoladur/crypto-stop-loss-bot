ef9a63bc83cc8c4e2610662530e8732f75239183
José María Sola Durán <josemaria.sola.duran@gmail.com>
2025-07-26 02:39:44 +0200

    Fixed bug

--- src/crypto_trailing_stop/infrastructure/adapters/dtos/bit2me_trading_wallet_balance.py
+++ src/crypto_trailing_stop/infrastructure/adapters/dtos/bit2me_trading_wallet_balance.py
@@ -11,8 +11,8 @@ class Bit2MeTradingWalletBalanceDto(BaseModel):
 
     @property
     def is_effective(self) -> bool:
-        # XXX: Minimal balance to consider potential losses is 0.1
-        return self.balance > 0.1 or self.blocked_balance > 0.1
+        # XXX: Minimal balance to consider potential losses is 0.01
+        return self.balance > 0.01 or self.blocked_balance > 0.01
 
     @property
     def total_balance(self) -> float | int:
