557c0ba41f8a3536dcb2e9c47aa4f0bf84d49aa8
José María Sola Durán <josemaria.sola.duran@gmail.com>
2025-08-25 17:44:20 +0200

    Removed category more robust since won't be used anymore

--- src/crypto_trailing_stop/scripts/vo.py
+++ src/crypto_trailing_stop/scripts/vo.py
@@ -15,7 +15,6 @@ class BacktestingExecutionResult:
 @dataclass
 class BacktestingExecutionSummary:
     best_overall: BacktestingExecutionResult | None = None
+    highest_quality: BacktestingExecutionResult | None = None
     best_profitable: BacktestingExecutionResult | None = None
     best_win_rate: BacktestingExecutionResult | None = None
-    highest_quality: BacktestingExecutionResult | None = None
-    most_robust: BacktestingExecutionResult | None = None
